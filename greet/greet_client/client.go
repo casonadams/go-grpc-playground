474f62be0131edfa0023da056cd52fd2595ea676
Cason Adams <casonadams@gmail.com>
2019-06-08 20:22:03 -0600

    simple working client server

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -21,7 +21,13 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Created Client: %f", c)
+	// fmt.Printf("Created Client: %f", c)
+
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do Unary RPC...")
 	req := &greetpb.GreetRequest{
 		Greeting: &greetpb.Greeting{
 			FirstName: "Cason",
@@ -29,5 +35,10 @@ func main() {
 		},
 	}
 
-	c.Greet(context.Background(), req)
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
+
 }
